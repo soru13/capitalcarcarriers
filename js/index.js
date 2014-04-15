790218a12d94ab6f75b72662cea2658e65ed7826
soru13 <jesus.ed13@gmail.com>
2014-04-14 20:59:43 -0600

    armando

---
+++ js/index.js
@@ -0,0 +1,51 @@
+var ajax=(function($){
+    function started(){ 
+    	$("#geocomplete").geocomplete()
+          .bind("geocode:result", function(event, result){
+            $.log("Result: " + result.formatted_address);
+          })
+          .bind("geocode:error", function(event, status){
+            $.log("ERROR: " + status);
+          })
+          .bind("geocode:multiple", function(event, results){
+            $.log("Multiple: " + results.length + " results found");
+        });
+
+    	$('#mapa').on('shown.bs.modal', function (e) {
+		  // do something...
+		  	var lat = 45.163779;                     
+	        var lng = -75.756359;  
+	        var position = new google.maps.LatLng(lat,lng);
+	        $('#map_canvas').gmap({ 'center': new google.maps.LatLng(lat, lng), 'zoom': 13});
+	        $('#map_canvas').gmap('addMarker', {'position': position}).click(function(){
+	                 $('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World! here is capital car carriers'}, this);
+	                 $('#map_canvas').gmap('getMap').panTo(position);
+	        });      
+		});
+		var defaultBounds = new google.maps.LatLngBounds(
+			var lat = position.coords.latitude;                     
+        var lng = position.coords.longitude;  
+		  new google.maps.LatLng(-33.8902, 151.1759),
+		  new google.maps.LatLng(-33.8474, 151.2631));
+
+		var input = document.getElementById('searchTextField');
+		var options = {
+		  bounds: defaultBounds,
+		  types: ['establishment']
+		};
+
+		autocomplete = new google.maps.places.Autocomplete(input, options);
+       
+    }
+         
+
+
+  return{
+    inicio:started,
+  }
+              
+})(jQuery);
+
+(function() {
+    $(document).on('ready',ajax.inicio);
+}).call(this);
